4c8b14bbba99ef180f91d109f9beb9d6d2fdda11
MAYET Tsiry <tsiry.mayet@irisa.fr>
2019-05-14 11:19:15 +0200

    projet initial

---
+++ Assets/Scenes/scripts/PipeManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeManager : MonoBehaviour {
+
+    public GameObject PipePrefab;
+    public int PipeCount;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+    public void DrawLevel()
+    {
+        ClearPipe();
+
+        Vector3[] directions = { Vector3.left, Vector3.up, Vector3.forward };
+
+        Vector3 currentPosition = Vector3.zero;
+        for (int i = 0; i < PipeCount; ++i)
+        {
+            Vector3 direction = directions[Random.Range(0, directions.Length)];
+            DrawPipe(currentPosition, currentPosition + direction);
+            currentPosition += direction;
+        }
+    }
+
+    public void ClearPipe()
+    {
+        List<Transform> childs = new List<Transform>();
+        foreach(Transform child in transform)
+        {
+            childs.Add(child);
+        }
+        foreach (Transform child in childs)
+        {
+            DestroyImmediate(child.gameObject);
+        }
+    }
+
+    private void DrawPipe(Vector3 v1, Vector3 v2)
+    {
+        Vector3 spawnPos = Vector3.Lerp(v1, v2, 0.5f);
+        float distance = Vector3.Distance(v1, v2);
+        GameObject instantiated = Instantiate(PipePrefab, spawnPos, Quaternion.identity);
+        instantiated.transform.localScale = new Vector3(0.5f, 0.5f, distance);
+        instantiated.transform.LookAt(v2);
+        instantiated.transform.parent = this.transform;
+    }
+
+}
