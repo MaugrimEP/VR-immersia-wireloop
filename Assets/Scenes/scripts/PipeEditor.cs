730b0f62dbdbcbc9707eb42ac8e456f898d9713c
qpetit <quentin.petit@irisa.fr>
2019-05-14 11:41:37 +0200

    Reup files not affected by .gitignore

---
+++ Assets/Scenes/scripts/PipeEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(PipeManager))]
+public class PipeEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        PipeManager pipeManager = (PipeManager)target;
+        if (GUILayout.Button("Draw Pipes"))
+        {
+            pipeManager.DrawLevel();
+        }
+    }
+}
