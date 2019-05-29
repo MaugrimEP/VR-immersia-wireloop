23dea91516a7522cf357251660307c34b585b79f
MAYET Tsiry <tsiry.mayet@irisa.fr>
2019-05-29 12:54:02 +0200

    v1 : collison backtrack

--- Assets/Torus/scripts/RaquetteController.cs
+++ Assets/Torus/scripts/RaquetteController.cs
@@ -12,6 +12,10 @@ public class RaquetteController : MonoBehaviour
     public InputController avatarInputController;
     public List<Renderer> renderers;
 
+    [Header("God Object collision param")]
+    public float maximalDisplacementUnity;
+    public float distanceStep;
+
     #region oldVar
 
     public VirtuoseManager vm;
@@ -129,8 +133,61 @@ public class RaquetteController : MonoBehaviour
             (Vector3 position, Quaternion rotation) = vm.Virtuose.Pose;
             (position, rotation) = V2UPosRot(position, rotation);
 
-            targetRigidbody.MovePosition(position);
-            targetRigidbody.MoveRotation(rotation);
+            Vector3 rigidbodyOldPosition = targetRigidbody.position;
+            Quaternion rigidbodyOldRotation = targetRigidbody.rotation;
+            float travelDistance = Vector3.Distance(rigidbodyOldPosition, position);
+
+            if (travelDistance < maximalDisplacementUnity)
+            {
+                targetRigidbody.MovePosition(position);
+                targetRigidbody.MoveRotation(rotation);
+            }
+            else
+            {
+
+                Debug.Log($"travelDistance < maximalDisplacementUnity : {travelDistance} < {maximalDisplacementUnity}");
+
+                Physics.autoSimulation = false;
+
+                //var declaration for the loop
+                bool previousIsCollided = infoCollision.IsCollided;
+                int stepNumber = Mathf.RoundToInt(travelDistance / distanceStep);
+                Vector3 previousPosition = rigidbodyOldPosition;
+                Quaternion previousRotation = rigidbodyOldRotation;
+                for (float i = 0; i<stepNumber; ++i)
+                {
+                    Vector3 stepEndPosition = Vector3.Lerp(rigidbodyOldPosition, position, i / stepNumber);
+                    Quaternion stepEndRotation = Quaternion.Lerp(rigidbodyOldRotation, rotation, i / stepNumber);
+                    targetRigidbody.MovePosition(stepEndPosition);
+                    targetRigidbody.MoveRotation(stepEndRotation);
+
+                    Physics.Simulate(Time.fixedDeltaTime);
+
+                    if (previousIsCollided && !infoCollision.IsCollided) // we just get out from a collision
+                    {
+
+                    }
+
+                    if(!previousIsCollided && infoCollision.IsCollided)// we just get into collision
+                    {// then we put the position as the previous position before the colliding
+                        vm.Virtuose.Pose = (previousPosition, previousRotation);
+
+                        lastFramePosition = position;
+                        lastFrameRotation = rotation;
+
+                        Debug.Log($"Displacement sized down from {travelDistance} to {Vector3.Distance(rigidbodyOldPosition, previousPosition)}");
+
+                        return;
+                    }
+
+                    previousPosition = stepEndPosition;
+                    previousRotation = stepEndRotation;
+                    previousIsCollided = infoCollision.IsCollided;
+                }
+
+                Physics.autoSimulation = true;
+            }
+
 
             float distance = 0;
             float dot = 0;
