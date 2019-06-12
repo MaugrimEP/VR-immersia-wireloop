05dc0af71dbb040c2a64632f2964a39c17be2a37
MAYET Tsiry <tsiry.mayet@irisa.fr>
2019-06-12 10:24:18 +0200

    add cooldown to AOEToggle

--- Assets/Torus/scripts/AOEController.cs
+++ Assets/Torus/scripts/AOEController.cs
@@ -5,11 +5,27 @@ using UnityEngine;
 public class AOEController : MonoBehaviour
 {
     public VirtuoseManager vm;
+    public float toggleDisplayCD = 1f;
+    private float nextToggleTime;
+
+    private void Start()
+    {
+        nextToggleTime = Time.time;
+    }
+
     private void Update()
     {
-        if (VRTools.GetKeyDown(KeyCode.A) || VRTools.IsButtonToggled(0) // manage the wand
-            || vm.IsButtonPressed(1))
-            ToggleChildDisplay();
+        if (VRTools.GetKeyDown(KeyCode.A) || VRTools.IsButtonToggled(0) // manage keyboard and wand
+            || vm.IsButtonToggled(1)                                    // manage virtuose
+            )
+        {
+            if (Time.time > nextToggleTime)
+            {
+                ToggleChildDisplay();
+                nextToggleTime = Time.time + toggleDisplayCD;
+            }
+        }
+
     }
 
     private void ToggleChildDisplay()
