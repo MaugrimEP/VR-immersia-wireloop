19915843c6656a046376b089786fd07325394663
MAYET Tsiry <tsiry.mayet@irisa.fr>
2019-06-18 15:20:24 +0200

    boucing ball

---
+++ Assets/Torus/scripts/BoucingBallController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoucingBallController : MonoBehaviour
+{
+    public InputController ic;
+    public GameObject BouncingBall;
+    public Transform spawnPoint;
+    public GameObject container;
+
+
+    private void Start()
+    {
+        SpawnBall();
+        /*
+        if (VRTools.IsClient())
+        {
+            this.enabled = false;
+            return;
+        }*/
+    }
+
+    void Update()
+    {
+        /*
+        if (ic.virtuoseManager.IsButtonPressed(1))
+        {
+            SpawnBall();
+        }
+        */
+    }
+
+    public void Clear()
+    {
+        foreach(Transform c in container.transform)
+        {
+            Destroy(c.gameObject);
+        }
+
+    }
+
+    private void SpawnBall()
+    {
+        Clear();
+        GameObject o = Instantiate(BouncingBall, spawnPoint.position, spawnPoint.rotation);
+        o.transform.localScale = Vector3.one * 0.1f;
+        o.transform.parent = container.transform;
+    }
+}
