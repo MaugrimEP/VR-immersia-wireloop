e06c8f8ce03dd6db976934026a0d062c1d4dc60c
MAYET Tsiry <tsiry.mayet@irisa.fr>
2019-06-21 09:14:21 +0200

    force rotation update

--- Assets/Torus/scripts/ReactionStr/ForceRotationStr.cs
+++ Assets/Torus/scripts/ReactionStr/ForceRotationStr.cs
@@ -1,63 +1,93 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
-/// <summary>
-/// Resolve the collision by using the force for the position, and using the last not colliding rotation to solve
-/// the rotation
-/// </summary>
+
 public class ForceRotationStr : IReactionStr
 {
+
     private Transform handleTransform;
-    private float stiffnessForce = 30f * 100f;
+    private float stiffnessForce;
+
     public ForceRotationStr(RaquetteController rc) : base(rc)
     {
         handleTransform = GameObject.Find("handlePosition").GetComponent<Transform>();
+        stiffnessForce = 1000f;// 30f * 100f / 4f;
     }
 
     public override void ComputeSimulationStep()
     {
-        (Vector3 READposition, Quaternion READrotation) = ic.GetVirtuosePoseRaw();
+        (Vector3 position, Quaternion rotation) = rc.ic.GetVirtuosePose();
 
-        rc.targetRigidbody.MovePosition(ic.GetVirtuosePose().Position);
-        rc.targetRigidbody.MoveRotation(ic.GetVirtuosePose().Rotation);
+        Vector3 oldPosition = rc.GetPosition();
+        Quaternion oldRotation = rc.GetRotation();
 
-        if (!rc.infoCollision.IsCollided)
+        //compute forces and torques
+        (Vector3 forces, Vector3 torques) = SolveForceAndTorque();
+        (Vector3 positionSolved, Quaternion rotationSolved) = SolvePositiondAndRotation();
+        forces = Utils.ClampVector3(forces, rc.MAX_FORCE);
+        torques = Utils.ClampVector3(torques, rc.MAX_TORQUE);
+
+        if (rc.infoCollision.IsCollided)
         {
-            ic.virtAddForce(Vector3.zero, Vector3.zero);
+            ic.SetVirtuosePose(positionSolved, rotationSolved);
+            ic.virtAddForce(forces, torques);
+        }
+        else
+        {
             ic.SetVirtuosePoseIdentity();
-            (rc.lastFramePosition, rc.lastFrameRotation) = (READposition, READrotation);
-            return;
+            ic.virtAddForce(Vector3.zero, Vector3.zero);
         }
 
-        (Vector3 solvedNextPosition, Quaternion solvedNextRotation) = SolvePositiondAndRotation();
-
-        Vector3 displacementClamped = Utils.ClampDisplacement(solvedNextPosition - READposition, rc.MAX_DISPLACEMENT);
-        solvedNextPosition = READposition + displacementClamped;
-
-        ic.SetVirtuosePoseRaw(solvedNextPosition, solvedNextRotation);
-
-    }
-
-    protected override (Vector3 Position, Quaternion Rotation) SolvePositiondAndRotation()
-    {
-
-        Vector3 normal = Vector3.Normalize(rc.target.transform.position - rc.targetRigidbody.position);
-
-        float interpenetrationDistance = Vector3.Distance(rc.target.transform.position, rc.targetRigidbody.position);
-        Vector3 forces = normal * interpenetrationDistance * stiffnessForce;
-        ic.virtAddForce(forces, Vector3.zero);
-
-        if (Debug.isDebugBuild) VectorManager.Clear();//TODO to remove : mode verbose
-        if (Debug.isDebugBuild) VectorManager.DrawVectorS(handleTransform.position, forces, Color.red, "force"); //TODO to remove : verbose
-        if (Debug.isDebugBuild) Debug.Log($"forces = {forces}");
-
-
-        return (ic.GetVirtuosePoseRaw().Position, rc.lastFrameRotation);
-
+        (rc.lastFramePosition, rc.lastFrameRotation) = (position, rotation);
     }
 
     protected override (Vector3 forces, Vector3 torques) SolveForceAndTorque()
     {
-        throw new System.NotImplementedException();
+        (Vector3 position, Quaternion rotation) = ic.GetVirtuosePose();
+
+        rc.targetRigidbody.MovePosition(position);
+        rc.targetRigidbody.MoveRotation(rotation);
+
+        if (!rc.IsColliding()) return (Vector3.zero, Vector3.zero);
+
+        Vector3 totalForce = Vector3.zero;
+
+        if (Debug.isDebugBuild) VectorManager.Clear();
+
+        foreach (ContactPoint contactPoint in currentCollision.contacts)
+        {
+            Vector3 vectorHandleContactPoint = contactPoint.point - handleTransform.position;
+            Vector3 normalToContact = contactPoint.normal;
+            float interpenetrationDistance = -contactPoint.separation;
+
+            Vector3 localForce = normalToContact * interpenetrationDistance * stiffnessForce;
+
+            totalForce += localForce;
+
+            if (Debug.isDebugBuild) Debug.DrawLine(handleTransform.position, contactPoint.point, Utils.RandomColor());
+            if (Debug.isDebugBuild) VectorManager.DrawSphereS(contactPoint.point, Vector3.one * 0.05f, Color.black);
+        }
+        if (Debug.isDebugBuild) Debug.Log($"contactPointCount = {currentCollision.contactCount}");
+        if (Debug.isDebugBuild) Debug.DrawLine(handleTransform.position, handleTransform.position + totalForce, Color.red);
+
+        return (totalForce, Vector3.zero);
+    }
+
+    protected override (Vector3 Position, Quaternion Rotation) SolvePositiondAndRotation()
+    {
+        (Vector3 virtPos, Quaternion virtRot) = ic.GetVirtuosePose();
+
+        if (!rc.IsColliding()) return (virtPos, virtRot);
+        else return (virtPos, rc.target.transform.rotation);
+    }
+
+    public override void HandleCollisionEnter(Collision collision)
+    {
+        base.HandleCollisionEnter(collision);
+    }
+
+    public override void HandleCollisionStay(Collision collision)
+    {
+        base.HandleCollisionStay(collision);
     }
 }
