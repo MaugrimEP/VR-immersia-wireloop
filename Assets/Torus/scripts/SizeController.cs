60dc2a251af437ba014684c758a26fc5ef67da81
MAYET Tsiry <tsiry.mayet@irisa.fr>
2019-07-04 11:41:50 +0200

    add raquette size controle with left and right key

---
+++ Assets/Torus/scripts/SizeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SizeController : MonoBehaviour
+{
+    public VRInput scaleUp;
+    public VRInput scaleDown;
+    public Transform raquetteTransform;
+    public float sizeStep;
+
+    void Update()
+    {
+        if (scaleUp.IsToggled() || VRTools.IsButtonToggled(4))
+        {
+            raquetteTransform.transform.localScale += Vector3.one * sizeStep;
+            VRTools.Log($"Size = {raquetteTransform.transform.localScale}");
+        }
+
+        if (scaleDown.IsToggled() || VRTools.IsButtonToggled(3))
+        {
+            raquetteTransform.transform.localScale -= Vector3.one * sizeStep;
+            VRTools.Log($"Size = {raquetteTransform.transform.localScale}");
+        }
+    }
+}
